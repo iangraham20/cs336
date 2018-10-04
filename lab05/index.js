5f7322a4368c922e45ff92efe1bbde47906de0b5
iangraham20 <igc2@students.calvin.edu>
2018-10-04 00:36:32 -0400

    finished homework1

--- lab05/index.js
+++ lab05/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const index = express()
 const port = 3000
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+index.get('/', (req, res) => res.send('Hello World!'))
+index.listen(port, () => console.log(`Example app listening on port ${port}!`))
