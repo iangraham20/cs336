2dd0d821e2f97c2cfdaf40bd8cd2fc306ec321ae
ian <igc2@students.calvin.edu>
2018-11-02 14:27:25 -0400

    worked through text file

--- lab06/app.js
+++ lab06/app.js
@@ -9,41 +9,20 @@ const express = require('express');			// Imports express
 const app = express();						// Initilizes an express application
 const port = 3000;							// Initilizes the port number
 
-//
-app.use(bodyParser.urlencoded({ extended: true }));	 //
-app.use(bodyParser.json());							 //
-app.use('/', express.static(__dirname + '/public')); //
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', express.static(__dirname + '/public'));
 
-/* Subroutine: 
- * Parameters:
- * Precondition:
- * Postcondition:
- */
 const writeResponseWithBody = (req, res, method) => {
   res.status(200);
   res.send(`Hello ${method} - ${JSON.stringify(req.body)}`);
 }
 
-/* Subroutine: 
- * Parameters:
- * Precondition:
- * Postcondition:
- */
 const writeResponse = (req, res, method) => {
   res.status(200);
   res.send(`Hello ${method}`);
 }
 
-//
-app.get('/request', (req, res) => writeResponse(req, res, 'GET'))			//
-.post('/request', (req, res) => writeResponseWithBody(req, res, 'POST'))	//
-.put('/request', (req, res) => writeResponseWithBody(req, res, 'PUT'))		//
-.delete('/request', (req, res) => writeResponse(req, res, 'DELETE'))		//
-.head('/request', (req, res) => writeResponse(req, res, 'HEAD'))			//
-.all('/request', (req, res) => res.sendStatus(501));						//
-
-//
 app.post('/forms', (req, res) => writeResponseWithBody(req, res, 'POST'));
 
-//
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
