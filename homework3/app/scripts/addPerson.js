559d528185c444c346f7630dbb5cd0e0337c9865
ian <igc2@students.calvin.edu>
2018-11-25 16:52:45 -0500

    completed the AJAX/JQuery webpages

---
+++ homework3/app/scripts/addPerson.js
@@ -0,0 +1,33 @@
+/* addPerson.js
+ *
+ * Author: Ian Christensen
+ * Prof: Vander Linden
+ * Class: CS336 at Calvin College
+ * Date: Fall, 2018
+ */
+
+$(document).ready(function() {
+	let form = $("form");
+	let result = $("#status");
+	form.change(function() {
+		result = "";
+	}
+	form.submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+		    type: form.attr('method'),
+		    url: form.attr('action'),
+		    data: form.serialize(),
+		})
+		.done(function(res) {
+			result.text("Success");
+			form.trigger("reset");
+		})
+		.fail(function(xhr, status, error) {
+			result.text("Error");
+			console.log("Error: " + error);
+			console.log("Status: " + status);
+			console.dir(xhr);
+		});
+	});
+});
---
+++ homework3/app/scripts/getPerson.js
@@ -0,0 +1,35 @@
+/* getPerson.js
+ *
+ * Author: Ian Christensen
+ * Prof: Vander Linden
+ * Class: CS336 at Calvin College
+ * Date: Fall, 2018
+ */
+
+ $(document).ready(function() {
+    let form = $("form");
+    let result = $("#result");
+    form.submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+		    type: form.attr('method'),
+		    url: form.attr('action') + 
+		    `/${(form.serializeArray())[0].value}`,
+		})
+	    .done(function(res) {
+		form.trigger("reset");
+		result.html(`
+                    <div id="result">
+                        <h1>${res.firstName} ${res.lastName}</h1>
+                        <div>Login ID: ${res.id}</div>
+                        <div>Start Date: ${res.startDate}</div>
+                    </div>`
+                );
+	    })
+	    .fail(function(xhr, status, error) {
+		console.log("Error: " + error);
+		console.log("Status: " + status);
+		console.dir(xhr);
+	    });
+    });
+});
